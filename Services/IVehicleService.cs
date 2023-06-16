209d20f54c7521c6857e011d61900c3be5e0d10f
shaqablanca <a.sameduz@gmail.com>
2023-06-17 00:07:31 +0200

    Read Data from Postgresql by using HttpGet method

--- Services/IVehicleService.cs
+++ Services/IVehicleService.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using EF7CodeFirst.Models;
+
 namespace EF7CodeFirst.Services
 {
-	public interface IVehicleService
-	{
-	}
+    public interface IVehicleService
+    {
+        Task<List<Vehicle>> GetAllVehicles();
+
+        Vehicle? GetVehicleById(int id);
+
+        List<Vehicle> AddVehicle(Vehicle vehicle);
+
+        List<Vehicle>? UpdateVehicle(int id, Vehicle modify);
+
+        List<Vehicle>? RemoveVehicle(int id);
+
+
+    }
 }
 
